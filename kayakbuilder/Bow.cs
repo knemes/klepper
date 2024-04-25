a165918c9510ce0545f6767e39f0ac4aad974a6d
Keaton Nemes <kknemes@gmail.com>
2024-04-24 19:17:19 -0500

    Update

--- kayakbuilder/Bow.cs
+++ kayakbuilder/Bow.cs
@@ -19,14 +19,13 @@ namespace kayakbuilder
         public Bow(double length, double height, double longweight, double vertweight)
         {
             //PARAMETRIC Bow Length
-            Point3d spoint = new Point3d(length, 0, 0);
-            Point3d spointB = new Point3d(-length, 0, 0);
-            Vector3d svector = spoint - spointB;
+            Point3d spoint = new Point3d(0, 0, 0);
+            Vector3d svector = new Vector3d(length, 0, 0);
 
             //PARAMETRIC Bow Height
-            Point3d epoint = new Point3d(0, 0, height);
-            Point3d epointB = new Point3d(0, 0, -height);
-            Vector3d evector = epointB - epoint;
+            Point3d epoint = new Point3d(length, 0, height);
+            Point3d epointB = new Point3d(length, 0, -height);
+            Vector3d evector = epoint - epointB;
 
             List<Point3d> points = new List<Point3d>();
             points.Add(spoint);
--- kayakbuilder/KayakBuilder.cs
+++ kayakbuilder/KayakBuilder.cs
@@ -27,20 +27,22 @@ namespace kayakbuilder
         {
             RhinoDoc.ActiveDoc.AdjustModelUnitSystem(UnitSystem.Inches, true);
 
-            int length = 16 * 12;
+            int klength = 16 * 12;
+            double slength = 6;
+            double blength = 12;
 
             //PARAMETRIC Boat Length
             Point3d origin = new Point3d(0, 0, 0);
-            Point3d kpoint = new Point3d(length, 0, 0);
-            LineCurve klength = new LineCurve(origin, kpoint);
+            Point3d kpoint = new Point3d(klength, 0, 0);
+            LineCurve centerline = new LineCurve(origin, kpoint);
 
-            Stern stern = new Stern(6, 9, 1);
+            Stern stern = new Stern(slength, 9, 1);
 
-            Bow bow = new Bow(12, 9, 1, 1);
-            Vector3d bowtranslate = new Vector3d(length, 0, 0);
+            Bow bow = new Bow(blength, 9, .1, 1);
+            Vector3d bowtranslate = new Vector3d(klength - blength, 0, 0);
             bow.bowcurve.Translate(bowtranslate);
 
-            doc.Objects.AddCurve(klength);
+            doc.Objects.AddCurve(centerline);
             doc.Objects.AddCurve(stern.sterncurve);
             doc.Objects.AddCurve(bow.bowcurve);
 
