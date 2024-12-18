0e003da48b91ed491380e1b3a6cbf07721e20e45
Keaton Nemes <kknemes@gmail.com>
2024-12-17 20:27:50 -0600

    Building gunwale

--- kayakbuilder/Gunwale.cs
+++ kayakbuilder/Gunwale.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using Rhino.Geometry.Collections;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace kayakbuilder
 {
-    internal class Class1
+    internal class Gunwale : KayakGeometry
     {
+        //Class Instance Variables
+        public string componenttype = "Gunwale";
+        public Curve gunwalecurve = null;
+
+        public Gunwale(double beam, double xplace, double height, Rhino.Geometry.Point3d sternpnt, Rhino.Geometry.Point3d bowpnt)
+        {
+            Point3d spoint = new Point3d(0, 0, 0);
+        }
     }
 }
